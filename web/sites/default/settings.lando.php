1d61ae0742155d355deb8ac019f54a9d82529a00
John Ouellet <john@thinktandem.io>
2019-05-24 08:55:30 -0400

    Update settings.lando.php

--- web/sites/default/settings.lando.php
+++ web/sites/default/settings.lando.php
@@ -18,10 +18,21 @@ if (isset($_SERVER['LANDO'])) {
     'port' => '3306',
     'driver' => 'mysql'
   ];
+  
+  // Only load dev settings on non admin pages.
+  // This prevents very slow loading admin pages.
+  // Especially when using some modules like layout builder.
+  if (strpos($_SERVER["REQUEST_URI"], '/admin/') === FALSE && php_sapi_name() !== 'cli') {
+    $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+    $settings['cache']['bins']['render'] = 'cache.backend.null';
+    $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+    $settings['cache']['bins']['page'] = 'cache.backend.null';
+    $config['system.performance']['css']['preprocess'] = FALSE;
+    $config['system.performance']['js']['preprocess'] = FALSE;
+  }
 
   // And a bogus hashsalt for now
   $settings['hash_salt'] = json_encode($databases);
-  
   // Fixes annoying perm issues on local.
   $settings['skip_permissions_hardening'] = TRUE;
 }
