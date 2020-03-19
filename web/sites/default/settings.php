aa5b8676e928fe2a3e3e971d561dee3e71454e6b
John Ouellet <john@thinktandem.io>
2020-03-19 09:47:11 -0400

    Update minimis + deps

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -46,3 +46,4 @@ if (file_exists($app_root . '/' . $site_path . '/settings.lando.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+$settings['config_sync_directory'] = '../config/sync';
